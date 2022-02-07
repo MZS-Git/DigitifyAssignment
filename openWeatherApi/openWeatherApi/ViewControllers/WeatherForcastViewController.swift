0cce98cbcc2d373425ebe2e88ce3dd79be9235e3
MZS-Git <ranazain397@gmail.com>
2022-02-07 20:25:30 +0500

    Project Setup

---
+++ openWeatherApi/openWeatherApi/ViewControllers/WeatherForcastViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  openWeatherApi
+//
+//  Created by Muhammad Zain Shahid on 07/02/2022.
+//
+
+import UIKit
+
+class WeatherForcastViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
